09daa6ec2467695954239ad79d923dae13c62a2e
Yi-Chia Chen <chenyichia.g@gmail.com>
2021-03-13 17:36:02 -0800

    1.1.0 added project name, hover highlight, easier level 1 list, and next list

--- js/lists.js
+++ js/lists.js
@@ -1,145 +1,281 @@
 // Yi-Chia Chen
-// speicail characters in tasks: ':', ';'
 
 var projects = [
     {
-        'name':'Taste Typicality (Andrew, Monica, Derek, Laurel, Brian)'
+        'name':'[taste] Taste Typicality (Andrew, Monica, Derek, Laurel, Brian)',
+        'acronym':'taste'
     },
     {
-        'name':'Aesthetics and Animacy (Frank)',
+        'name':'[aniAes] Aesthetics and Animacy (Frank)',
+        'acronym':'aniAes',
         'next':[
             'head/left foot -- M(x/y position) from 2 repeats x 80 Ss = 160 trials x 20 walkers -- correlate with ratings of aesthetics/animacy/residuals',
             'PB&R if rejected from Cognition'
         ]
     },
     {
-        'name':'Elegant Movements (Frank)',
+        'name':'[emoAes] Elegant Movements (Frank)',
+        'acronym':'emoAes',
         'next':[
             'check aesEmo additional analysis and organize summary',
             'Run aesEmo analysis in experiment 1',
             'modeling for both experiments',
             'run naturalness experiment',
             'write this in the paper -- Amodal effect from emotion recognition vs. perceptual effect from perceptual cues for emotions',
-            'read more about facial attractiveness and emotion: happy > neutral > sad > angry?; ',
-            'future experiments: AQ; Relationship with "dynamic/movement" in artworks (e.g., Mondrian, Pollock, dot techniques; aesthetics in art vs. in daily life)'
+            'read more about facial attractiveness and emotion: happy > neutral > sad > angry?',
+            'future experiments: AQ, Relationship with "dynamic/movement" in artworks (e.g., Mondrian, Pollock, dot techniques, aesthetics in art vs. in daily life)'
         ]
     },
     {
-        'name':'PsyOMN (Felix, Jaime)',
+        'name':'[PsyONN] (Felix, Jaime)',
+        'acronym':'PsyONN',
         'next':[
-            'schedule another meeting for pilot experiments',
             'IRB',
             'learn pyTorch',
-            'potential collaborators: Shuhao pattern extraction from waveform; Kellman perceptual learning'
+            'collaborators? Shuhao pattern extraction from waveform, Kellman perceptual learning'
         ]
     },
     {
-        'name':'Autistic Traits & Taste Study (Moriah)',
+        'name':'[autTaste] Autistic Traits & Taste Study (Moriah)',
+        'acronym':'autTaste',
         'next':[
-            'exploratory analysis: Connect words chosen with Wordnet for semantic analysis; Consider ImageNet and described scene databases Nick is using in future experiments; What attributes are in this image (checkboxes) to classify'
+            'exploratory analysis: Connect words chosen with Wordnet for semantic analysis',
+            'Consider ImageNet and described scene databases Nick is using in future experiments',
+            'What attributes are in this image (checkboxes) to classify'
         ]
     },
     {
-        'name':'Conservative Aesthetic Taste (Moriah)',
+        'name':'[conservTaste] Conservative Aesthetic Taste (Moriah)',
+        'acronym':'conservTaste',
         'next':[
-            'cross-cultural differences: Tsai, J. L. (2007) Ideal affect: Cultural causes and behavioral consequences. Perspectives on Psychological Science 19:242–59.',
+            'cross-cultural differences -- Tsai, J. L. (2007) Ideal affect: Cultural causes and behavioral consequences. Perspectives on Psychological Science 19:242–59.',
             'work memory',
             'IQ test 16 questions (fluid intelligence)',
             'metaphor approximate creatitivity (crystalized intelligence)'
         ]
     },
     {
-        'name':'Social Avoidance (Clara & Shuhao)',
+        'name':'[socAvo] Social Avoidance (Clara & Shuhao)',
+        'acronym':'socAvo',
         'next':[
             'potential conclusion -- spontaneous/implicit stimuli encoding bias',
             'Examined factors like duration, acceleration, distance from target, later movement toward destination'
         ]
     },
     {
-        'name':'Social Avoidance Modeling (Clara & Shuhao)',
+        'name':'[socAvoModel] Social Avoidance Modeling (Clara & Shuhao)',
+        'acronym':'socAvoModel',
         'next':[
             'program new experiment with Shuhao',
-            "Direction: Other's attention effects on other social entities are represented in a third party as social force (not physical force); Idea: Detecting social avoidance involves matching the social force distribution from observed attention of first party to the observed movements of the second party; We can test what is the distribution among orientation and distance, strengh of the force, whether such social force follow physical law or other norm (different law? no law? different parameters/constants?)"
+            "Direction: Other's attention effects on other social entities are represented in a third party as social force (not physical force); Idea: Detecting social avoidance involves matching the social force distribution from observed attention of first party to the observed movements of the second party, We can test what is the distribution among orientation and distance, strengh of the force, whether such social force follow physical law or other norm (different law? no law? different parameters/constants?)"
         ]
     },
     {
-        'name':'Modeling Typical Scene for Aesthetics (Shuhao & Xiaoyang)',
+        'name':'[typScene] Modeling Typical Scene for Aesthetics (Shuhao & Xiaoyang)',
+        'acronym':'typScene',
         'next':[
             'meet as a group to discuss next analysis',
-            'GAIN network: pass raw image; perturbation; Aesthetic judgment'
+            'GAIN network: pass raw image, perturbation, Aesthetic judgment'
         ]
     },
     {
-        'name':'Self-non-biological-motion Recognition (Akila)',
-        'next':['program new experiment']
+        'name':'[selfRec] Self-non-biological-motion Recognition (Akila)',
+        'acronym':'selfRec',
+        'next':['program new experiment by the end of March']
     },
     {
-        'name':'Presentation Principle (Idan Blank?)',
+        'name':'[presntPrinc] Presentation Principle (Idan Blank?)',
+        'acronym':'presntPrinc',
         'next':['present ideas in lab meeting','ask Idan for collaboration']
     },
     {
-        'name':'NIH NRSA (by 8/8)',
-        'next':['Taylor Webb ask for NRSA materials']
+        'name':'[NRSA] NIH NRSA by 8/8 or 12/8; BRAIN letter 7/10 | 8/10',
+        'acronym':'NRSA',
+        'deadline':'8/8 or 12/8 (BRAIN letter of intent 7/10 | 8/10)',
+        'descriptions':[
+            "<a target='_blank' href='https://grants.nih.gov/grants/guide/pa-files/PA-21-048.html'>info</a>",
+            "<a target='_blank' href='https://grants.nih.gov/grants/guide/notice-files/NOT-EY-21-001.html'>Diversity info</a>",
+            "<a target='_blank' href='https://grants.nih.gov/grants/guide/rfa-files/RFA-MH-20-620.html'>BRAIN Initiative version</a>"
+        ]
     },
     {
-        'name':'K99'
+        'name':'[K99] by 10/6 or 2022.2/10 or 2022.6/8',
+        'acronym':'K99',
+        'deadline':'10/6 or 2022.2/10 or 2022.6/8',
+        'descriptions':["<a target='_blank' href='https://grants.nih.gov/grants/guide/rfa-files/RFA-NS-19-043.html'>info</a>"]
     },
     {
-        'name':'method paper presentation for next quarter'
+        'name':'[K01] by 10/12',
+        'acronym':'K01',
+        'deadline':'10/12',
+        'descriptions':[
+            "<a target='_blank' href='https://grants.nih.gov/grants/guide/pa-files/PA-20-190.html'>info 1</a>",
+            "<a target='_blank' href='https://grants.nih.gov/grants/guide/pa-files/PA-20-191.html'>info 2</a>",
+        ]
+    },
+    {
+        'name':'[lab] method paper presentation for next quarter',
+        'acronym':'lab'
     }
 ];
 
 var next_actions = [
-    {'name':'[selfRec] think about new design'},
-    {'name':'[emoAes] Add more analyses (see today.txt on desktop)'},
-    {'name':'[tasteTypicality] Write NHB'},
-    {'name':'check K99 deadlines and timeout for both general and disability types'},
-    {'name':'check NIH/NSF grant call deadline'},
-    {'name':"plan out <a target='_blank' href='https://grants.nih.gov/grants/policy/nihgps/HTML5/section_11/11.2.2_eligibility.htm'>NRSA</a> timeline"},
+    {
+        'name':'[selfRec] read papers',
+        'acronym':'selfRec',
+        'deadline':'next monday (3/15)'
+    },
+    {
+        'name':'[emoAes] Add more analyses (see today.txt on desktop)',
+        'acronym':'emoAes'
+    },
+    {
+        'name':'[taste] Write NHB',
+        'acronym':'taste'
+    },
+    {
+        'name':'check NIH/NSF grant call deadline',
+        'acronym':'others'
+    },
+    {
+        'name':"[NRSA] plan out <a target='_blank' href='https://grants.nih.gov/grants/policy/nihgps/HTML5/section_11/11.2.2_eligibility.htm'>NRSA</a> timeline",
+        'acronym':'NRSA'
+    },
     {
         'name':'[aniAes] Rewrite the paper for Cognition',
-        'descriptions':'rewrite the introduction as Watching people -> impression -> statics -> dynamics; Add more about everyday experience (add more dance studies in the intro); Add the systematic factors determining apporachable'
+        'acronym':'aniAes',
+        'descriptions':[
+            'rewrite the introduction as Watching people -> impression -> statics -> dynamics',
+            'Add more about everyday experience (add more dance studies in the intro)',
+            'Add the systematic factors determining apporachable'
+        ]
     },
     {
         'name':"Write letter of intent for <a target='_blank' href='https://tdg.ucla.edu/UCLA-innovation-fund'>Innovation Fund 2021</a>",
-        'deadline':'May 1, 2021'
+        'acronym':'others',
+        'deadline':'5/1'
     },
-    {'name':'decide on papers to present for method meeting'},
-    {'name':'[presentPrinciple] ideas generation'},
-    {'name':"[typicalScene] look at Xiaoyang's result files"}
+    {
+        'name':'[lab] decide on papers to present for method meeting',
+        'acronym':'lab'
+    },
+    {
+        'name':'[presntPrinc] ideas generation',
+        'acronym':'presntPrinc'
+    },
+    {
+        'name':"[typScene] look at Xiaoyang's result files",
+        'acronym':'typScene'
+    },
+    {
+        'name': '[NRSA] Taylor Webb ask for NRSA materials',
+        'acronym':'NRSA'
+    },
+    {
+        'name': '[fMRIAes] analyze the second sample',
+        'acronym':'fMRIAes',
+        'descriptions':[
+            'Find out how Sasha did her working memory correlation with complexity preference',
+            'Measure stimuli complexity and individual complexity preference scores',
+            'add a column to latest taste typicality sheet and Send it to Monica, Taylor, and Anna'
+        ]
+    },
+    {
+        'name':'file tax return',
+        'acronym':'others',
+        'deadline':'4/15'
+    }
 ];
 
 var waiting_for = [
-    {'name':'[autTaste] Run between-subject experiment next quarter'},
-    {'name':'[conservTaste] Moriah is analyzing data now'},
+    {
+        'name':'[autTaste] Run between-subject experiment next quarter',
+        'acronym':'autTaste'
+    },
+    {
+        'name':'[conservTaste] Moriah is analyzing data now',
+        'acronym':'conservTaste'
+    },
     {
         'name':"[socAvo] Program between-subject experiment after getting Shuhao's parameters",
-        'descriptions':'Multiple choice experiment: testing orientation/position x red/blue; maybe add other filler trials to make sure subjects are still engaging socially?; 7 options forced choice (tick marks appeared around the disappeared blue guy); Position question -- tip position along the moving direction; multiple trials; video mirrored and inverted to create multiple versions; reduce playing time for blue triangle; Record accuracy and errors'
+        'acronym':'socAvo',
+        'descriptions':[
+            'Multiple choice experiment:',
+            'testing orientation/position x red/blue',
+            'maybe add other filler trials to make sure subjects are still engaging socially?',
+            '7 options forced choice (tick marks appeared around the disappeared blue guy)',
+            'Position question -- tip position along the moving direction',
+            'multiple trials',
+            'video mirrored and inverted to create multiple versions',
+            'reduce playing time for blue triangle',
+            'Record accuracy and errors'
+        ]
     },
-    {'name':'[socAvoModel] Meet for desiging wider range experiment for next quarter'},
-    {'name':'[typicalScene] Xiaoyang is working on the analysis'}
+    {
+        'name':'[socAvoModel] Meet for desiging wider range experiment for next quarter',
+        'acronym':'socAvoModel'
+    },
+    {
+        'name':'[typScene] Xiaoyang is working on the analysis',
+        'acronym':'typScene'
+    },
+    {
+        'name':'[typMem] Taste Typicality predicts Memory Typicality?',
+        'acronym':'typMem',
+        'descriptions':[
+            'Ask Qi about collaboration',
+            "whether taste typicality is related to people's typicality in image memory:",
+            'tell us (a) unusual encoding leads to unusual taste, or (b) unusual taste leads to unusual encoding',
+            'some images afford more routes (various cues) for encoding, and may lead to both high memorability and low consensus in aesthetics'
+        ]
+    }
 ];
 
 var someday = [
     {
-        'name':'Representation of Social Interaction (Tianmin)',
-        'descriptions': "Think about what aspect of memory should be tested; Tianmin Shu's stimuli (Can we yoked the conditions?); postively-valenced/cooperative social interaction -> memory?"
+        'name':'[socInt] Representation of Social Interaction (Tianmin)',
+        'acronym':'socInt',
+        'descriptions': [
+            "Think about what aspect of memory should be tested",
+            "Tianmin Shu's stimuli (Can we yoked the conditions?)",
+            "postively-valenced/cooperative social interaction -> memory?"
+        ]
     },
     {
-        'name':'Inward Bias & Autistic Traits (Clara & Sophie)',
-        'descriptions':'Need to add gaze deflection in and revise the code for Prolific and run'
+        'name':'[inAut] Inward Bias & Autistic Traits (Clara & Sophie)',
+        'acronym':'inAut',
+        'descriptions':[
+            'Need to add gaze deflection in and revise the code for Prolific and run',
+            'Sophie: human subject training & add to IRB'
+        ]
     },
     {
-        'name':'Modeling Biological Motion',
-        'descriptions':'based on Ryali_etal_2020_faceTypicalityAttractiveness; Model for point-light walkers just like the 90/50-axis model in faces; Statistical typicality of the point-light walkers predict aesthetic experience?; Trait judgments and identify features (create database, manipulate it, etc.)'
+        'name':'[BMModel] Modeling Biological Motion',
+        'acronym':'BMModel',
+        'descriptions':[
+            'based on Ryali_etal_2020_faceTypicalityAttractiveness',
+            'Model for point-light walkers just like the 90/50-axis model in faces',
+            'Statistical typicality of the point-light walkers predict aesthetic experience?',
+            'Trait judgments and identify features (create database, manipulate it, etc.)'
+        ]
     },
-    {'name':'program online timer management app'},
     {
-        'name':'Social Cues for Conversations',
-        'descriptions':"social cues that signal the enjoyment of conversation and its effect on the content, length of coversation, enjoyment of your partner, rating of the partner's inattention"
+        'name':'program online timer management app',
+        'acronym':'others'
     },
     {
-        'name':'Framing vs. partially occluded effect in preference for scene',
-        'descriptions':'How does framing influence preference of scenes?; Is it really about framing as in taking photo or creating pictures (artistic knowledge)?; Or is it about avoidance of occlusion/out of view (evolutionary explanations)?; Manipulation -- framing vs. partial occlusion, where the visible part of the image is identical'
+        'name':'[socConv] Social Cues for Conversations',
+        'acronym':'socConv',
+        'descriptions':["social cues that signal the enjoyment of conversation and its effect on the content, length of coversation, enjoyment of your partner, rating of the partner's inattention"]
+    },
+    {
+        'name':'[FrmOcc] Framing vs. partially occluded effect in preference for scene',
+        'acronym':'FrmOcc',
+        'descriptions':[
+            'How does framing influence preference of scenes?',
+            'Is it really about framing as in taking photo or creating pictures (artistic knowledge)?',
+            'Or is it about avoidance of occlusion/out of view (evolutionary explanations)?',
+            'Manipulation -- framing vs. partial occlusion, where the visible part of the image is identical'
+        ]
     }
 ];
--- js/main.js
+++ js/main.js
@@ -1,58 +1,86 @@
 // Yi-Chia Chen
 
 // planned improvement:
-// 1. select by keyboard
-// 2. move by select and drag
-// 3. click to add to google calendar
+// 1. select by clicking and up down left right keys
+//     a. keep visible
+//     b. action menu: add to google calendar, copy text, (required server to edit) delete
+//     c. (required server to edit) allow dragging (show trach bin, other list light up when hover, line indicating where it'd be inserted, drop on item to add a sublist)
+//     d. (required server to edit) plus sign show up for adding items
+// 2. use database instead
+// 3. plus sign for adding items
+//     a. clicking the sign call a pop up menue for items
 
-function CREATE_NEW_UNORERED_LIST(parent_element,level,id) {
-    parent_element.append("<ul class='level"+level+"-list' id="+id+'></ul>');
+function CREATE_NEW_UNORERED_LIST(parent_element,id) {
+    parent_element.append("<ul class='level1-list' id="+id+'></ul>');
     return $('#'+id);
 }
 
-function ADD_LIST_ITEM(list_element,item_text,item_id) {
-    list_element.append('<li id='+item_id+'>'+item_text+'</li>');
+function ADD_LIST_ITEM(list_element,item_text,item_class, item_id) {
+    item_text = item_text.replace('[', "<span class='project-name'>[").replace(']', "]</span>");
+    list_element.append("<li class='"+item_class+"' id='"+item_id+"'>"+item_text+'</li>');
     return $('#'+item_id);
 }
 
+function ADD_LEVEL_0_ITEMS(list_el, item_obj, index) {
+    let el = ADD_LIST_ITEM(list_el, item_obj.name, item_obj.acronym, list_el.attr('id')+'-item-'+index);
+    if (typeof item_obj.deadline !== 'undefined') {
+        el.attr('title', item_obj.deadline);
+        el.addClass('deadline');
+    }
+}
+
+function ADD_DESCRIPTIONS_ITEMS(list_el, item_obj, index) {
+    let level1_list = CREATE_NEW_UNORERED_LIST(list_el, list_el.attr('id')+'-level1-'+index);
+    for (let j=0; j<item_obj.descriptions.length; j++){
+        ADD_LIST_ITEM(level1_list, item_obj.descriptions[j], 'others', level1_list.attr('id')+'-item-'+index+'-'+j);
+    }
+}
+
+function ADD_NEXT_ITEMS(list_el, item_obj, index) {
+    let level1_list = $('#'+list_el.attr('id')+'-level1-'+index);
+    if (level1_list.length == 0) {
+        level1_list = CREATE_NEW_UNORERED_LIST(list_el, list_el.attr('id')+'-level1-'+index);
+    }
+    for (let j=0; j<item_obj.next.length; j++){
+        ADD_LIST_ITEM(level1_list, item_obj.next[j], 'others', level1_list.attr('id')+'-item-'+index+'-next-'+j);
+    }
+}
+
+
 function LOOP_FOR_ADDING_ITEMS(list, list_element) {
     for (let i=0; i<list.length; i++) {
-        let now_element = ADD_LIST_ITEM(list_element, list[i].name, list_element.attr('id')+'-item-'+i);
-        if (typeof list[i].deadline !== 'undefined') {
-            now_element.attr('title', list[i].deadline);
-            now_element.attr('class', 'pink');
-        }
+        ADD_LEVEL_0_ITEMS(list_element, list[i], i);
 
         if (typeof list[i].descriptions !== 'undefined') {
-            let level1_list = CREATE_NEW_UNORERED_LIST(list_element, 1, list_element.attr('id')+'-list-'+i);
-            let level1_descriptions = list[i].descriptions.split(':');
-
-            if (level1_descriptions.length > 1) {
-                for (let j=0; j<level1_descriptions.length; j+=2){
-                    ADD_LIST_ITEM(level1_list, level1_descriptions[j], level1_list.attr('id')+'-item-'+i);
-                    let level2_list = CREATE_NEW_UNORERED_LIST(level1_list, 2, level1_list.attr('id')+'-list-'+j);
-                    let level2_descriptions = level1_descriptions[j+1].split('; ');
-                    if (level2_descriptions.length>1){
-                        for (let k=0; k<level2_descriptions.length; k++) {
-                            ADD_LIST_ITEM(level2_list, level2_descriptions[k], level2_list.attr('id')+'-item-'+k);
-                        }
-                    }
-                }
-            } else {
-                level1_descriptions = level1_descriptions[0].split('; ');
-                for (let j=0; j<level1_descriptions.length; j++){
-                    ADD_LIST_ITEM(level1_list, level1_descriptions[j], level1_list.attr('id')+'-item-'+j);
-                }
-            }
+            ADD_DESCRIPTIONS_ITEMS(list_element, list[i], i);
         }
 
+        if (list_element.attr('id') == 'projects-list') {
+            if (typeof list[i].next !== 'undefined') {
+                ADD_NEXT_ITEMS(list_element, list[i], i);
+            }
+        }
     }
 }
 
 // display lists
 $(document).ready(function() {
+
     LOOP_FOR_ADDING_ITEMS(projects, $('#projects-list'));
     LOOP_FOR_ADDING_ITEMS(next_actions, $('#next-actions-list'));
     LOOP_FOR_ADDING_ITEMS(waiting_for, $('#waiting-for-list'));
     LOOP_FOR_ADDING_ITEMS(someday, $('#someday-list'));
+
+    $('li').hover(
+        function() {
+            const CLASSES = $(this).attr('class');
+            const PROJECT_NAME = CLASSES.replace('deadline','').replace(' ','');
+            if (PROJECT_NAME !== 'others'){
+                $('.'+PROJECT_NAME).addClass('highlight-project');
+            }
+        }, function() {
+            $('li').removeClass('highlight-project');
+        }
+    );
+
 });
